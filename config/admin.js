2ee94ab401a4f58a81130afe8c588a690e125158
Mayanksinghhh <singhmayank56132@gmail.com>
2025-09-08 11:10:02 +0530

    final commit

---
+++ config/admin.js
@@ -0,0 +1,24 @@
+module.exports = ({ env }) => ({
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
+  secrets: {
+    encryptionKey: env('ENCRYPTION_KEY'),
+  },
+  flags: {
+    nps: env.bool('FLAG_NPS', true),
+    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+  },
+  // Disable analytics to prevent ERR_BLOCKED_BY_CLIENT errors
+  analytics: {
+    enabled: false,
+  },
+});
